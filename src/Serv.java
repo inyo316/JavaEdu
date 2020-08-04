e11e1e3f9ac7e9759f5ad5fe12d831bbc14b6527
inyo316 <inyo316@naver.com>
2020-08-04 16:30:13 +0900

    jiny

---
+++ src/Serv.java
@@ -0,0 +1,57 @@
+//package myServlet;
+ 
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+/**
+ * Servlet implementation class Serv
+ */
+//@WebServlet("/ajaxCon")
+public class Serv extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Serv() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+ 
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        response.setContentType("application/x-json; charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        String name = request.getParameter("name");
+        String age = request.getParameter("age");    
+        
+        System.out.println("doget :"+name);
+        System.out.println("deget :"+age);
+            
+        out.print("Get 통신 : 안녕 내 이름은 "+name+"이고 나이는 "+age+"란다"); //response    
+    }
+ 
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        response.setContentType("application/x-json; charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        String name = request.getParameter("name");
+        String age = request.getParameter("age");    
+        
+        System.out.println("doget :"+name);
+        System.out.println("deget :"+age);
+            
+        out.println("Post통신 : 안녕 내 이름은 "+name+"이고 나이는 "+age+"란다"); //response
+    }
+}
+//dfdfdsfs
